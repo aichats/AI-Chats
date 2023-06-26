d7f141ecba1792dad7606b922533ad6a6303fa7d
Hiro <laciferin@gmail.com>
2023-06-26 15:44:38 +0530

    Revert "wip: kidding with refactor"
    This reverts commit e3f9c557816179e95ab8880709075a868ea892f4.

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import path from "path";
 export default defineConfig({
   build: {
     outDir: "build",
-    rollupOptions: {
-      input: '/public/index.html',
-    },
   },
   plugins: [react()],
   resolve: {
